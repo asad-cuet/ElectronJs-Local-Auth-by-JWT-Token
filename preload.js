4d75d1fb79a33d4f4e7fb2d480f5d8b38dabbf7f
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2025-05-09 03:18:18 +0600

    up

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('authAPI', {
     notify: async (title,body) => ipcRenderer.invoke('notify', { title,body }),
-    user: async (token) => ipcRenderer.invoke('user'),
+    user: async () => ipcRenderer.invoke('user'),
     login: async (email, password) => ipcRenderer.invoke('login', { email, password }),
     register: async (email, password) => ipcRenderer.invoke('register', { email, password }),
     logout: async (token) => ipcRenderer.invoke('logout'),
